8796d35f38faffe37157706020d6ee04f54169ea
sebjvidal <seb.vidal@btinternet.com>
2023-06-20 17:34:01 -0500

    Initial Commit

---
+++ TextStyleHelper/TextStyleHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextStyleHelperApp.swift
+//  TextStyleHelper
+//
+//  Created by Seb Vidal on 20/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct TextStyleHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
